f9d118afa13d9538db645b109635ee9a70c3c067
Makera <makera@makera.local>
2022-01-15 17:35:52 +0100

    first commit from makera

---
+++ src/main/java/com/chompfooddeliveryapp/ChompFoodDeliveryAppApplication.java
@@ -0,0 +1,13 @@
+package com.chompfooddeliveryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChompFoodDeliveryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChompFoodDeliveryAppApplication.class, args);
+    }
+
+}
