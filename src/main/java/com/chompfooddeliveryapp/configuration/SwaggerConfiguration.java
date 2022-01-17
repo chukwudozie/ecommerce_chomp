4aabf093fbcfa4860c142066100f931225e2dbb1
Kintttt <79329326+Kintttt@users.noreply.github.com>
2022-01-17 22:30:46 +0100

    refactors the swagger config class

--- src/main/java/com/chompfooddeliveryapp/configuration/SwaggerConfiguration.java
+++ src/main/java/com/chompfooddeliveryapp/configuration/SwaggerConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -19,6 +20,7 @@ public class SwaggerConfiguration implements WebMvcConfigurer {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.chompfooddeliveryapp"))
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiDetails());
     }
