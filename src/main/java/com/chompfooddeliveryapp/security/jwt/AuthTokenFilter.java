db5242c905718d696a28f8ec7cc2e9cf207f14c0
Emeka <emkychuks062@gmail.com>
2022-01-18 16:52:34 +0100

    change UserDetailImpl and AuthTokenFilter

--- src/main/java/com/chompfooddeliveryapp/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/chompfooddeliveryapp/security/jwt/AuthTokenFilter.java
@@ -1,5 +1,8 @@
 package com.chompfooddeliveryapp.security.jwt;
 
+import com.chompfooddeliveryapp.security.service.UserDetailsServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -9,6 +12,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final JwtUtils utils;
+
+    private static  final Logger LOGGER = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    public AuthTokenFilter(UserDetailsServiceImpl userDetailsService, JwtUtils utils) {
+        this.userDetailsService = userDetailsService;
+        this.utils = utils;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request,
          HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
--- src/main/java/com/chompfooddeliveryapp/security/service/UserDetailsImpl.java
+++ src/main/java/com/chompfooddeliveryapp/security/service/UserDetailsImpl.java
@@ -1,26 +1,59 @@
 package com.chompfooddeliveryapp.security.service;
 
+import antlr.collections.impl.LList;
+import com.chompfooddeliveryapp.model.enums.UserGender;
+import com.chompfooddeliveryapp.model.enums.UserRole;
+import com.chompfooddeliveryapp.model.users.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 public class UserDetailsImpl implements UserDetails {
 
     private Long id;
-    private String email;
+    private String firstName;
+    private String lastName;
+    private UserGender gender;
+
     @JsonIgnore
     private String password;
-    private Collection<? extends  GrantedAuthority> authorities;
+    private Boolean subscribed;
 
-    public UserDetailsImpl(Long id, String email,
-      String password, Collection<? extends GrantedAuthority> authorities) {
+    private String email;
+
+    private UserRole authorities;
+
+    public UserDetailsImpl(Long id, String firstName, String lastName,
+                           UserGender  gender, String password, Boolean subscribed,
+                           String email,UserRole authorities) {
         this.id = id;
-        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
         this.password = password;
+        this.subscribed = subscribed;
+        this.email = email;
         this.authorities = authorities;
     }
+
+
+    public static UserDetails build(User user) {
+        UserRole authorities = user.getRole();
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getGender(),
+                user.getPassword(),
+                user.getSubscribed(),
+                user.getEmail(),
+                authorities
+        );
+    }
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -29,31 +62,31 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
