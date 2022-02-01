d43aa971e1bf3166e93ff1c6b3be7fb6e722fb4a
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-02-01 23:31:06 +0100

    creates OrderDetailsRepository

--- src/main/java/com/chompfooddeliveryapp/repository/OrderDetailsRepository.java
+++ src/main/java/com/chompfooddeliveryapp/repository/OrderDetailsRepository.java
@@ -1,11 +1,9 @@
 package com.chompfooddeliveryapp.repository;
 
-import com.chompfooddeliveryapp.model.orders.Order;
 import com.chompfooddeliveryapp.model.orders.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public interface OrderDetailsRepository extends JpaRepository<OrderDetail, Long> {
 
@@ -15,5 +13,6 @@ public interface OrderDetailsRepository extends JpaRepository<OrderDetail, Long>
     Optional<OrderDetail> findOrderDetailById(Long orderId);
 
     List<OrderDetail> findAllByOrderId(Long orderId);
+    Optional<List<OrderDetail>> findAllByOrderIdEquals(Long orderId);
 }
 
