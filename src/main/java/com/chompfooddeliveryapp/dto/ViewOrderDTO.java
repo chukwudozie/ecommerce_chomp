c480dac1c5343b1d182268e35b1a8d5dfa55e5c5
Amy-Oji <amyojiakor@gmail.com>
2022-02-01 01:42:02 +0100

    removes comments

--- src/main/java/com/chompfooddeliveryapp/dto/ViewOrderDTO.java
+++ src/main/java/com/chompfooddeliveryapp/dto/ViewOrderDTO.java
@@ -21,25 +21,3 @@ public class ViewOrderDTO {
     private Timestamp delivered_date;
     private String transaction;
 }
-
-
-
-//    dateOrdered
-// endpoint to view details of a particular order
-//My Orders: As a registered user, I want to see all my orders so that I can manage my order
-// history and keep track of new orders.
-//
-//Acceptance Criteria:
-//should be able to see details of only orders the user has made.
-//Should be able to see the following for each order:
-//Image, quantity, date ordered, date delivered, status
-//Status should be either- Delivered, pending or canceled
-// payment details per order
-//Item’s total
-//Delivery fee
-//Total
-//Delivery method
-//Shipping address
-//Status of each order should be automatically updated.
-//Should be able to directly order any delivered items again
-//Should be able to return a delivered item
