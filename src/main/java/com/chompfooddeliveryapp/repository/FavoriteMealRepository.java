49b8b6b1e0ec0d9e5e5e730bdabc70e1af1b12ba
Muna Ganobi <nwabundoonyekwelu@gmail.com>
2022-02-01 17:28:06 +0100

    worked on the controller

--- src/main/java/com/chompfooddeliveryapp/repository/FavoriteMealRepository.java
+++ src/main/java/com/chompfooddeliveryapp/repository/FavoriteMealRepository.java
@@ -5,10 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface FavoriteMealRepository extends JpaRepository<FavoriteMeal, Long> {
+
+    @Query("select u from FavoriteMeal u where u.menu_id = ?1 and u.user_id = ?2")
     boolean findByMenu_idAndUser_id(Long menuId, Long user_id);
 
+    @Query("select u from FavoriteMeal u where u.user_id = ?1")
     List<FavoriteMeal> findByUser_id(Long userId);
+
+    @Query("select u from FavoriteMeal u where u.user_id = ?1 and u.menu_id = ?2")
+   Optional<FavoriteMeal> findFavoriteMealByUser_idAndMenu_id(Long userId, long mealId);
 }
--- src/main/java/com/chompfooddeliveryapp/service/serviceInterfaces/FavoriteMealService.java
+++ src/main/java/com/chompfooddeliveryapp/service/serviceInterfaces/FavoriteMealService.java
@@ -1,13 +1,15 @@
 package com.chompfooddeliveryapp.service.serviceInterfaces;
 
 import com.chompfooddeliveryapp.model.meals.FavoriteMeal;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface FavoriteMealService {
     FavoriteMeal createFavoriteMeal(FavoriteMeal favoriteMeal);
 
-    void deleteFavoriteMeal(Long id);
+    String deleteFavoriteMeal(Long mealId);
 
     public List<FavoriteMeal> getAllFavoriteMealsByAUser(Long userId);
 
