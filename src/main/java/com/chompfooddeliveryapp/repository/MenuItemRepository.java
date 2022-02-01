218549765342d34893289876c6352e73aed46d3e
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-02-01 23:17:06 +0100

    creates MenuItemRepository

--- src/main/java/com/chompfooddeliveryapp/repository/MenuItemRepository.java
+++ src/main/java/com/chompfooddeliveryapp/repository/MenuItemRepository.java
@@ -3,6 +3,7 @@ package com.chompfooddeliveryapp.repository;
 import com.chompfooddeliveryapp.model.meals.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface  MenuItemRepository extends JpaRepository<MenuItem, Long> {
+
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 
 }
