e65c7d0ba463f6bf9a8cad6a1f288fc69dd6143f
Odochi-Okara <85240161+Odochi-Okara@users.noreply.github.com>
2022-02-02 14:16:47 +0100

    created test for MenuServiceImplementation

--- src/main/java/com/chompfooddeliveryapp/repository/MenuItemRepository.java
+++ src/main/java/com/chompfooddeliveryapp/repository/MenuItemRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long>{
 
     Optional<MenuItem> findMenuItemById(Long id);
-    void deleteEmployeeById(Long id);
+    void deleteMenuItemById(Long id);
 
 }
