d1386a8d4456f6cd042f34860bf7d5ca09d8ef35
Elvis Ikenna <elvisikenna07@gmail.com>
2022-02-07 20:59:03 +0100

    modifies CheckoutService class and CartService Class

--- src/main/java/com/chompfooddeliveryapp/service/serviceInterfaces/CheckoutService.java
+++ src/main/java/com/chompfooddeliveryapp/service/serviceInterfaces/CheckoutService.java
@@ -3,6 +3,7 @@ package com.chompfooddeliveryapp.service.serviceInterfaces;
 import com.chompfooddeliveryapp.dto.ResponseViewUserOrdersDTO;
 import com.chompfooddeliveryapp.dto.ShippingAddressDTO;
 import com.chompfooddeliveryapp.model.meals.MenuItem;
+import com.chompfooddeliveryapp.payload.AllCartItems;
 
 import java.util.List;
 
@@ -10,4 +11,5 @@ public interface CheckoutService {
     List<MenuItem> getOrderSummary(Long userId);
     ResponseViewUserOrdersDTO getAllOrdersByUserId(Long userId);
     String saveShippingAddress(long userId, ShippingAddressDTO shippingAddressDTO);
+    AllCartItems checkoutCartItems(Long userId);
 }
