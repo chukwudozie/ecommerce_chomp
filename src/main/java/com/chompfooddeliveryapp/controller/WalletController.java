880f35d87484d60be0c94f2d9a786cc953854eb5
makera <makera.apiri@decagon.dev>
2022-02-08 00:47:39 +0100

    putting secret into application properties

--- src/main/java/com/chompfooddeliveryapp/controller/WalletController.java
+++ src/main/java/com/chompfooddeliveryapp/controller/WalletController.java
@@ -25,29 +25,19 @@ import static com.chompfooddeliveryapp.model.enums.PaymentMethod.*;
 import static com.chompfooddeliveryapp.model.enums.TransactionType.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/user/wallet")
 //@RequiredArgsConstructor
 public class WalletController {
 
 
-    private final String secret_key = "sk_test_2d5c43445c023f91a8f13334df77580390a395c9";
-
-    private final WebClient.Builder webClient;
     private final WalletServiceImpl walletService;
-    private final TransactionRepository transactionRepository;
-    private final UserRepository userRepository;
-    private final ObjectMapper objectMapper;
     private final TransactionServiceImpl transactionService;
     private final PaystackServiceImpl paystackService;
 
-    public WalletController(WebClient.Builder webClient, WalletServiceImpl walletService,
-                            TransactionRepository transactionRepository, UserRepository userRepository,
-                            ObjectMapper objectMapper, TransactionServiceImpl transactionService, PaystackServiceImpl paystackService) {
-        this.webClient = webClient;
+    public WalletController( WalletServiceImpl walletService, TransactionServiceImpl transactionService,
+                             PaystackServiceImpl paystackService) {
+
         this.walletService = walletService;
-        this.transactionRepository = transactionRepository;
-        this.userRepository = userRepository;
-        this.objectMapper = objectMapper;
         this.transactionService = transactionService;
         this.paystackService = paystackService;
     }
@@ -64,7 +54,7 @@ public class WalletController {
     }
 
     @PostMapping("/verifytransaction")
-    public ResponseEntity<?> PayStackDto(@RequestBody VerifyTransactionDto verifyTransactionDto) throws IOException {
+    public ResponseEntity<?> PayStackDto(@RequestBody VerifyTransactionDto verifyTransactionDto) throws Exception {
 
         PayStackResponseDto responseDto = paystackService.verifyPaystackTransaction(verifyTransactionDto);
 
--- src/main/java/com/chompfooddeliveryapp/service/serviceImpl/PaystackServiceImpl.java
+++ src/main/java/com/chompfooddeliveryapp/service/serviceImpl/PaystackServiceImpl.java
@@ -10,7 +10,6 @@ import com.chompfooddeliveryapp.model.users.User;
 import com.chompfooddeliveryapp.repository.UserRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,16 +23,15 @@ public class PaystackServiceImpl {
     private final UserRepository userRepository;
     private final WebClient.Builder webClient;
     private final ObjectMapper objectMapper;
+    private final String secret;
 
-
-    @Value("${chompfood.app.paystackSecret}")
-    private String secret;
-
-    public PaystackServiceImpl(TransactionServiceImpl transactionService, UserRepository userRepository, WebClient.Builder webClient, ObjectMapper objectMapper) {
+    public PaystackServiceImpl(TransactionServiceImpl transactionService, UserRepository userRepository,
+                               WebClient.Builder webClient, ObjectMapper objectMapper,@Value("${paystack.Secret}") String secret) {
         this.transactionService = transactionService;
         this.userRepository = userRepository;
         this.webClient = webClient;
         this.objectMapper = objectMapper;
+        this.secret = secret;
     }
 
 
