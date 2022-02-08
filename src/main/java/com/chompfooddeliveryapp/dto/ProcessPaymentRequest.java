860611dc89b19ff6a88edcc016f4ac17e190b6da
Emeka <emkychuks062@gmail.com>
2022-02-08 21:51:59 +0100

    create ProcessPaymentRequest class

---
+++ src/main/java/com/chompfooddeliveryapp/dto/ProcessPaymentRequest.java
@@ -0,0 +1,17 @@
+package com.chompfooddeliveryapp.dto;
+
+import com.chompfooddeliveryapp.model.enums.PaymentMethod;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProcessPaymentRequest {
+
+    private String paymentMethod;
+    private Long amount;
+}
