f4a1847bdcd8371e753ae322dae4ef1a4642c16c
James Okonkwo <68653373+Onyetech@users.noreply.github.com>
2022-02-10 23:16:20 +0100

    create payment service test class

---
+++ src/test/java/com/chompfooddeliveryapp/service/serviceImpl/PaymentServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.chompfooddeliveryapp.service.serviceImpl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void processPayment() {
+    }
+
+    @Test
+    void verifyPayStackPayment() {
+    }
+}
