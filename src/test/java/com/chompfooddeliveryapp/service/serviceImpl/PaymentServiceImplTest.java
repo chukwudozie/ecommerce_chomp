3b0bd920d0196653c3ec3c0687bdb23482ff2c0f
James Okonkwo <68653373+Onyetech@users.noreply.github.com>
2022-02-10 23:20:22 +0100

    modify the payment service test class

--- src/test/java/com/chompfooddeliveryapp/service/serviceImpl/PaymentServiceImplTest.java
+++ src/test/java/com/chompfooddeliveryapp/service/serviceImpl/PaymentServiceImplTest.java
@@ -1,11 +1,33 @@
 package com.chompfooddeliveryapp.service.serviceImpl;
 
+import com.chompfooddeliveryapp.repository.OrderRepository;
+import com.chompfooddeliveryapp.repository.TransactionRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class PaymentServiceImplTest {
+
+    @Mock
+    PaystackServiceImpl payStackService;
+
+    @Mock
+    WalletWithdrawServiceImpl withdrawService;
+
+    @Mock
+    TransactionRepository transactionRepository;
+
+    @Mock
+    OrderRepository orderRepository;
+
+    @InjectMocks
+    PaymentServiceImpl paymentService;
 
     @BeforeEach
     void setUp() {
