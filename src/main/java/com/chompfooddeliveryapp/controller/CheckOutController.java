cb8dca982113785643ce0f41662767c004bffdd8
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-02-11 22:38:19 +0100

    modifies CheckOutController class

--- src/main/java/com/chompfooddeliveryapp/controller/CheckOutController.java
+++ src/main/java/com/chompfooddeliveryapp/controller/CheckOutController.java
@@ -2,23 +2,28 @@ package com.chompfooddeliveryapp.controller;
 
 import com.chompfooddeliveryapp.dto.ShippingAddressDTO;
 import com.chompfooddeliveryapp.payload.AllCartItems;
+import com.chompfooddeliveryapp.service.serviceInterfaces.CheckoutService;
 import com.chompfooddeliveryapp.service.serviceInterfaces.ViewOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class CheckOutController {
 
     private final ViewOrderService viewOrderService;
+    private final CheckoutService checkoutService;
 
     @Autowired
-    public CheckOutController(ViewOrderService viewOrderService) {
+    public CheckOutController(ViewOrderService viewOrderService, CheckoutService checkoutService) {
         this.viewOrderService = viewOrderService;
+        this.checkoutService = checkoutService;
     }
 
     @GetMapping("/{userId}/checkout")
@@ -28,6 +33,11 @@ public class CheckOutController {
     }
 
     public ResponseEntity<List<ShippingAddressDTO>> getUserAddress(@PathVariable long userId) {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body(checkoutService.getAllAddress(userId));
+    }
+
+    @GetMapping("/new/order/{userId}/{cartId}")
+    public ResponseEntity<?> createOrder(@PathVariable("userId") long userId, @PathVariable("cartId") long cartId) {
+        return ResponseEntity.ok().body(checkoutService.createOrderFromCartItem(userId, cartId));
     }
 }
