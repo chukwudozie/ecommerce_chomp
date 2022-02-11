a58f9ceb3b3cbea6fb987f8caadcc5927e513eb5
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-02-11 23:52:48 +0100

    modifies CheckoutServiceImpl

--- src/main/java/com/chompfooddeliveryapp/service/serviceImpl/CheckoutServiceImpl.java
+++ src/main/java/com/chompfooddeliveryapp/service/serviceImpl/CheckoutServiceImpl.java
@@ -1,7 +1,6 @@
 package com.chompfooddeliveryapp.service.serviceImpl;
 
 
-import com.chompfooddeliveryapp.dto.OrderSummaryDTO;
 import com.chompfooddeliveryapp.dto.ShippingAddressDTO;
 import com.chompfooddeliveryapp.model.carts.Cart;
 import com.chompfooddeliveryapp.model.carts.CartItem;
