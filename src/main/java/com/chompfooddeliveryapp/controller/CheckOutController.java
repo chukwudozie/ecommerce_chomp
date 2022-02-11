2187c43a693e4126b69cc80b1e34a3f46fab506c
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-02-11 14:18:17 +0100

    Modifies Checkoutcontroller

--- src/main/java/com/chompfooddeliveryapp/controller/CheckOutController.java
+++ src/main/java/com/chompfooddeliveryapp/controller/CheckOutController.java
@@ -1,10 +1,9 @@
 package com.chompfooddeliveryapp.controller;
 
-import com.chompfooddeliveryapp.model.meals.MenuItem;
+import com.chompfooddeliveryapp.dto.ShippingAddressDTO;
 import com.chompfooddeliveryapp.payload.AllCartItems;
-import com.chompfooddeliveryapp.service.serviceInterfaces.CheckoutService;
+import com.chompfooddeliveryapp.service.serviceInterfaces.ViewOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,15 +14,20 @@ import java.util.List;
 @RestController
 public class CheckOutController {
 
-    private final CheckoutService checkoutService;
+    private final ViewOrderService viewOrderService;
 
-    public CheckOutController(CheckoutService checkoutService) {
-        this.checkoutService = checkoutService;
+    @Autowired
+    public CheckOutController(ViewOrderService viewOrderService) {
+        this.viewOrderService = viewOrderService;
     }
 
     @GetMapping("/{userId}/checkout")
     public ResponseEntity<AllCartItems> checkoutOrders(@PathVariable("userId") long userId) {
-        var checkoutItems = checkoutService.checkoutCartItems(userId);
+        var checkoutItems = viewOrderService.checkoutCartItems(userId);
         return ResponseEntity.ok().body(checkoutItems);
+    }
+
+    public ResponseEntity<List<ShippingAddressDTO>> getUserAddress(@PathVariable long userId) {
+        return ResponseEntity.ok().build();
     }
 }
