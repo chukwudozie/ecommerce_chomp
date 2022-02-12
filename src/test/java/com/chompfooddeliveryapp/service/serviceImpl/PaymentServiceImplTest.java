5933463e5462275380b00c84842ba2b44c3d04e3
Emeka <emkychuks062@gmail.com>
2022-02-12 21:54:18 +0100

    test for Payment service implementation

--- src/test/java/com/chompfooddeliveryapp/service/serviceImpl/PaymentServiceImplTest.java
+++ src/test/java/com/chompfooddeliveryapp/service/serviceImpl/PaymentServiceImplTest.java
@@ -1,8 +1,12 @@
 package com.chompfooddeliveryapp.service.serviceImpl;
 
 import com.chompfooddeliveryapp.dto.PayStackRequestDto;
+import com.chompfooddeliveryapp.dto.ProcessPaymentRequest;
 import com.chompfooddeliveryapp.dto.WithdrawalRequest;
+import com.chompfooddeliveryapp.model.enums.PaymentMethod;
+import com.chompfooddeliveryapp.model.enums.TransactionStatus;
 import com.chompfooddeliveryapp.model.enums.TransactionType;
+import com.chompfooddeliveryapp.model.orders.Order;
 import com.chompfooddeliveryapp.model.users.User;
 import com.chompfooddeliveryapp.repository.OrderRepository;
 import com.chompfooddeliveryapp.repository.TransactionRepository;
@@ -12,6 +16,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,14 +41,16 @@ class PaymentServiceImplTest {
     PaymentServiceImpl paymentService;
 
     User user;
+    Order order;
 
 
     @BeforeEach
     void setUp() {
         user = new User();
+        order = new Order();
         PayStackRequestDto requestDto = new PayStackRequestDto();
         requestDto.setAmount(700);
-        payStackService.initializePaystackTransaction(requestDto, user.getId(), TransactionType.DEBIT);
+//        payStackService.initializePaystackTransaction(requestDto, user.getId(), TransactionType.DEBIT);
 
         WithdrawalRequest walletWithdraw = new WithdrawalRequest();
         walletWithdraw.setBill(700);
@@ -51,6 +60,13 @@ class PaymentServiceImplTest {
 
     @Test
     void processPayment() {
+        user.setId(1L);
+        order.setId(1L);
+        when(orderRepository.findOrderByIdAndUserId(user.getId(),1L)).thenReturn(Optional.of(order));
+        ProcessPaymentRequest paymentRequest = new ProcessPaymentRequest();
+        paymentRequest.setPaymentMethod(PaymentMethod.EWALLET.name());
+        paymentRequest.setAmount(300L);
+        paymentService.processPayment(paymentRequest,user.getId(),1L);
     }
 
     @Test
