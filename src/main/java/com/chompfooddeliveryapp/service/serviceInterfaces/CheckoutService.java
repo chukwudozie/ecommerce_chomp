ddce927f8f7be481a47428a8f6f51cd91c0bbc1e
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-02-12 13:26:50 +0100

    modifies CheckoutService class

--- src/main/java/com/chompfooddeliveryapp/service/serviceInterfaces/CheckoutService.java
+++ src/main/java/com/chompfooddeliveryapp/service/serviceInterfaces/CheckoutService.java
@@ -1,8 +1,6 @@
 package com.chompfooddeliveryapp.service.serviceInterfaces;
 
 import com.chompfooddeliveryapp.dto.ShippingAddressDTO;
-import com.chompfooddeliveryapp.model.orders.Order;
-import com.chompfooddeliveryapp.model.users.ShippingAddress;
 import com.chompfooddeliveryapp.model.users.User;
 import com.chompfooddeliveryapp.payload.response.CheckoutResponse;
 import com.chompfooddeliveryapp.payload.response.ProductSummary;
@@ -10,7 +8,7 @@ import com.chompfooddeliveryapp.payload.response.ProductSummary;
 import java.util.List;
 
 public interface CheckoutService {
-    ShippingAddress saveShippingAddress(long userId, ShippingAddressDTO shippingAddressDTO);
+    List<String> saveShippingAddress(long userId, ShippingAddressDTO shippingAddressDTO);
     CheckoutResponse createOrderFromCartItem(long userId, long cartId);
     List<ProductSummary> getAllCartItems(long userId, long cartId);
     ShippingAddressDTO getDefaultShippingAddress(User user);
