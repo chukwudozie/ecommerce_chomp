385926e816dc4e3e34d224860eb4358d59247461
makera <makera.apiri@decagon.dev>
2022-03-02 09:53:37 +0100

    "user order controller path fix"

--- src/main/java/com/chompfooddeliveryapp/controller/UserOrderController.java
+++ src/main/java/com/chompfooddeliveryapp/controller/UserOrderController.java
@@ -31,9 +31,9 @@ public class UserOrderController {
     }
 
 
-    @GetMapping("/viewOrderDetails/{userId}/{orderId}")
-    public ResponseEntity<?> viewOrderDetails(@PathVariable("userId") Long userId, @PathVariable("orderId") Long orderId){
-        return new ResponseEntity<>(orderService.getOrderDetails(userId, orderId), HttpStatus.OK);
+    @GetMapping("/viewOrderDetails/{orderId}")
+    public ResponseEntity<?> viewOrderDetails( @PathVariable("orderId") Long orderId){
+        return new ResponseEntity<>(orderService.getOrderDetails(userService.getUserIDFromSecurityContext(), orderId), HttpStatus.OK);
     }
 
     @PostMapping("/shipping-address")
